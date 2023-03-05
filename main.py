5face43dbc053c196740f73a065cff6b6c512dfb
AlexP04 <84740734+AlexP04@users.noreply.github.com>
2023-03-05 13:01:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ st.title('The method of cross-influence analysis')
 
 col1, col2 = st.columns(2)
 
-probabilities_file = col1.file_uploader('Input', type=['xlsx'], key='input_file')
-weights_file = col1.file_uploader('Weights', type=['xlsx'], key='weights_file')
+probabilities_file = col1.file_uploader('Estimations input', type=['xlsx'], key='input_file')
+weights_file = col1.file_uploader('Weights input', type=['xlsx'], key='weights_file')
+num_iterations = col1.number_input('Number of iterations', value=100, step=1, key='number_of_iterations')
 
 if col2.button('Run', key='run'):
     if (probabilities_file is None) or (weights_file is None):
@@ -34,9 +35,20 @@ if col2.button('Run', key='run'):
         ]
         estimations = pd.read_excel(probabilities_file)
         weights = pd.read_excel(weights_file)
-
-#         solution = Solution()
-        dataframes = [weights.head(), weights.head(), weights.head(), weights.head(), weights.head(), estimations.head(), estimations.head()]
+        p = {
+            'm':weights.shape[0],
+            'n':estimations.shape[0],
+            'weights': weights,
+            'estimations': estimations
+        }
+        
+        solution = Solution(p)
+        solution.initialize()
+        solution.process(num_iterations = num_iterations, vocab = False)
+        l1, l2, l3, l4, P = solution.estimate_truth_coeficient(vocab = False)
+        
+        dataframes = [solution.aprior_probabilities, solution.new_aprior_probabilities, l1, l2, l3, l4, P]
+        
         for i in range(7):
             res_cols[i].write(col_names[i])
             res_cols[i].dataframe(
